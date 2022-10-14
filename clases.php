e9cf1d473020154805c6de254db0c5ffc2a2e16c
Galo98-N <galonahuel98@gmail.com>
2022-10-14 18:09:16 -0300

    Iniciando el repo

---
+++ clases.php
@@ -0,0 +1,49 @@
+<?php 
+
+    class Cliente extends Persona{
+
+        // Atributos
+        protected $nroCliente;
+        protected $direccion;
+        protected $telefono;
+
+        // Constructor
+        public function __construct($nombre,$apellido,$dni,$nroCliente,$direccion,$telefono){
+            parent::__construct($nombre,$apellido,$dni);
+            $this->nroCliente = $nroCliente;
+            $this->direccion = $direccion;
+            $this->telefono = $telefono;
+        }
+
+        // Metodos
+        
+    }
+
+        class Cliente extends Persona{
+
+        // Atributos
+        protected $nroCliente;
+        protected $direccion;
+        protected $telefono;
+
+        // Constructor
+        public function __construct($nombre,$apellido,$dni,$nroCliente,$direccion,$telefono){
+            parent::__construct($nombre,$apellido,$dni);
+            $this->nroCliente = $nroCliente;
+            $this->direccion = $direccion;
+            $this->telefono = $telefono;
+        }
+
+        // Metodos
+        
+        
+    }
+
+
+
+
+
+
+
+
+?>
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Ejercicio PHP con objetos
